ca3976a9c4b8a95e66e9267d71a54f4ca13e9da3
mcneney <mcneney@sfu.ca>
2016-09-21 13:15:53 -0700

    Add group selection script.

---
+++ groups.R
@@ -0,0 +1,10 @@
+groups <- c(
+  "Lillian, Will, Yue, Richard, Michelle, Sarah",
+  "Shufei, Jinwan, Payman, Yueren, Jing, Di, Faezeh",
+  "Ran, Grace",
+  "Nate, Saad, Xiao, Jacob")
+
+# Homework 1: first two sampled groups present Chapter 1, problem 23,
+# last two present prosecutor's fallacy
+set.seed(1)
+sample(groups)
